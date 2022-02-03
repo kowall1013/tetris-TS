c96318468941e90aeafe84940bd79ceb9fa6fed0
mateusz.kowalski <mateusz.kowalski@trustmate.io>
2022-02-03 07:40:05 +0100

    initial project

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { createGlobalStyle } from 'styled-components';
+import bgImage from './img/bg.jpg';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
+const GlobalStyles = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    background: url(${bgImage});
+    background-size: cover;
+    background-position: center;
+    height: 100vh;
+  }
+`;
+
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyles />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
